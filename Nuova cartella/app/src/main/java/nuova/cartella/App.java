961e7ddef4c19da9388acf286e3a4a45899d1367
Nicola <n.dipinto8@studenti.uniba.it>
2022-07-25 18:26:37 +0200

    aggiornato main e classi

--- Nuova cartella/app/src/main/java/nuova/cartella/App.java
+++ Nuova cartella/app/src/main/java/nuova/cartella/App.java
@@ -14,19 +14,20 @@ public class App {
 
 
 
-        Calciatore Totti=new Calciatore("Francesco",38,"Attaccante");
-        Totti.setGol(22);
-        Totti.setPresenze(30);
-        Totti.setSquadra("Roma","Olimpico");
-        System.out.println(Totti.getNome());
-        System.out.println(Totti.getEta());
-        System.out.println(Totti.getRuolo());
-        System.out.println(Totti.getGol());
-        System.out.println(Totti.getPresenze());
-        System.out.println(Totti.getSquadra());
+        Calciatore Ronaldo=new Calciatore("Cristiano","Ronaldo",38,"Attaccante");
+        Ronaldo.setGol(815);
+        Ronaldo.setPresenze(1118);
+        Ronaldo.setSquadra("Manchester United","Civitas Metropolitano");
+        Ronaldo.bioCalciatore();
 
-        
+        Calciatore Messi=new Calciatore ("Lionel","Messi",36,"Attaccante");
+        Messi.setGol(774);
+        Messi.setPresenze(995);
+        Messi.setSquadra("PSG","Parco dei Principi");
+        Messi.bioCalciatore();
 
+        Messi.setSquadra("Napoli","Stadio Diego Armando Maradona");
+        Messi.bioCalciatore();
 
        
 
--- Nuova cartella/app/src/main/java/nuova/cartella/Calciatore.java
+++ Nuova cartella/app/src/main/java/nuova/cartella/Calciatore.java
@@ -2,19 +2,34 @@ package nuova.cartella;
 
 public class Calciatore {
     private String nome;
+    private String cognome;
     private int eta;
     private String ruolo;
     private int presenze;
     private int gol;
     private Squadra squadra;              ////////
 
-    Calciatore(String n,int e,String r){
+    Calciatore(String n,String c,int e,String r){
         nome=n;
+        cognome=c;
         eta=e;
         ruolo=r;
     }
+    public void setPresenze(int p){
+        this.presenze=p;
+    }
+    public void setGol(int g){
+        this.gol=g;
+    }
+    public void setSquadra(String name_team,String name_stadium){
+        this.squadra=new Squadra(name_team,name_stadium);
+    }
+
     public String getNome(){
         return this.nome;
+    }
+    public String getCognome(){
+        return this.cognome;
     }
     public int getEta(){
         return this.eta;
@@ -31,14 +46,15 @@ public class Calciatore {
     public String getSquadra(){          ////////
         return this.squadra.getNomeSquadra();
     }
-    public void setPresenze(int p){
-        this.presenze=p;
-    }
-    public void setGol(int g){
-       this. gol=g;
-    }
-    public void setSquadra(String name_team,String name_stadium){
-        this.squadra=new Squadra(name_team,name_stadium);
-    }
 
+    public void bioCalciatore(){
+        System.out.println("\n"+"Biografia di "+ nome+" "+cognome);
+        System.out.println("Nome: "+nome);
+        System.out.println("Cognome: "+cognome);
+        System.out.println("Eta': "+eta);
+        System.out.println("Ruolo: "+ruolo);
+        System.out.println("Presenze: "+presenze);
+        System.out.println("Gol: "+gol);
+        System.out.println("Squadra attuale: "+squadra.getNomeSquadra());
+    }
 }
